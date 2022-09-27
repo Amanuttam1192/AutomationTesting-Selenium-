76a0336c55ec3c206af05b8cb96787134e5b7ce3
Aman uttam <81846308+Amanuttam1192@users.noreply.github.com>
2022-09-27 10:26:09 +0530

    innitial commit
    First automation testing of a contact form,which automates responses at any extent.

---
+++ AutomationBeginning/src/ContactBox.java
@@ -0,0 +1,18 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class ContactBox {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.setProperty("webdriver.chrome.driver.exe", "C:\\Users\\amanu\\Downloads\\chromedriver_win32\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://amanuttam1192.github.io/#contact");
+		driver.findElement(By.id("name")).sendKeys("Ashok");
+		driver.findElement(By.id("email")).sendKeys("ash.kvuc@gmail.com");
+		driver.findElement(By.id("message")).sendKeys("hil hil ke nacho nacho");
+		driver.findElement(By.className("primary")).click();
+	}
+
+}
