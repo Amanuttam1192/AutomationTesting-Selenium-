0593a49e1a61ab08d8d194b04b3d01376b67980f
Aman uttam <81846308+Amanuttam1192@users.noreply.github.com>
2022-10-21 08:19:39 +0530

    Add files via upload

---
+++ aman.java
@@ -0,0 +1,20 @@
+public class aman{
+		public static void main(String args[]){
+name luli=new name();
+luli.name="uttam ji";
+system.out.println(luli.name);
+
+
+
+
+
+}
+}
+class name(){
+ string naam;
+}
+
+
+
+
+
---
+++ first.java
@@ -0,0 +1,21 @@
+public class first{
+public static void main(String[] args){
+System.out.print("hil hil ke nacho nacho");
+
+nacho person1= new nacho();
+nacho person2= new nacho();
+person1.age=21;
+person1.name="aman";
+System.out.print(person1.age + " " + person1.name);
+
+}
+
+}
+class nacho{
+int age;
+String name;
+}
+
+
+
+
